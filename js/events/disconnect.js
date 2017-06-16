39e361a647148c92ec24e0fbd293f4e7b578cb22
O\Oscar <theoscar@protonmail.com>
2017-06-16 22:27:15 +0100

    Start ELSint rewrite

--- js/events/disconnect.js
+++
@@ -1,7 +0,0 @@
-const main = require('../../eye.js');
-
-main.client.on('disconnect', async (closeEvent) => {
-	if (closeEvent.code === 1000) {
-		process.exit();
-	}
-});
--- js/events/guildmemberupdate.js
+++
@@ -1,9 +0,0 @@
-const main = require('../../eye.js');
-
-const util = require('../util.js');
-
-main.client.on('guildMemberUpdate', async (guildMemberOld, guildMemberNew) => {
-	if (guildMemberOld.displayName !== guildMemberNew.displayName) {
-		util.checkGuildName(guildMemberNew, guildMemberNew.guild);
-	}
-});
