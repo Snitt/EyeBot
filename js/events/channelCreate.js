0b4740c1ed3f5094e774098df5cb482d0c00fb9a
O\Oscar <theoscar@protonmail.com>
2017-06-18 00:53:11 +0100

    Add channelCreate event

--- js/events/channelCreate.js
+++ js/events/channelCreate.js
@@ -1,3 +1,14 @@
-module.exports = (channel) => {
+const data = require('../data')
+const util = require('../util')
 
+module.exports = async (channel) => {
+  let isGuildChannel = (channel.type === 'text' || channel.type === 'voice')
+
+  if (isGuildChannel) {
+    try {
+      await data.checkChannel(channel)
+    } catch (error) {
+      util.logError('channelCreate', error)
+    }
+  }
 }
